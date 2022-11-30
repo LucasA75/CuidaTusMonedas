b91c75533e69954caef44aa437222d1d9dc09a61
abrilonia <abrilbracamonte@gmail.com>
2022-11-30 10:15:08 -0300

    cambios en monedero

---
+++ demo/src/main/java/com/ctm/demo/controller/MonederoController.java
@@ -0,0 +1,48 @@
+package com.ctm.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ctm.demo.model.Monedero;
+import com.ctm.demo.service.MonederoService;
+
+
+
+@CrossOrigin("*")
+@RestController
+public class MonederoController {
+    private MonederoService monederoService;
+
+    public MonederoController(@Autowired MonederoService monederoService){
+        this.monederoService = monederoService;
+    }
+
+    @PostMapping("/Monedero/save")
+    public void guardarMonedero(@RequestBody Monedero monedero){
+        monederoService.guardarMonedero(monedero);
+    }
+    
+    @PutMapping("/Monedero/update")
+    public void updateMonedero(@RequestBody Monedero monedero){
+        monederoService.updateMonedero(monedero);
+    }
+
+    @GetMapping("/Monedero/findAll")
+    public List<Monedero> allMonedero(){
+        return monederoService.allMonedero();
+    }
+
+    @DeleteMapping("/Monedero/{id}")
+    public void eliminarMonedero (@PathVariable Integer id){
+        monederoService.eliminarMonedero(id);
+    }
+}
---
+++ demo/src/main/java/com/ctm/demo/service/MonederoService.java
@@ -0,0 +1,37 @@
+package com.ctm.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ctm.demo.model.Monedero;
+import com.ctm.demo.repository.MonederoRepository;
+
+
+
+@Service
+@Transactional
+public class MonederoService {
+    private MonederoRepository monederoRepository;
+
+    public MonederoService(MonederoRepository monederoRepository){
+        this.monederoRepository = monederoRepository;
+    }
+
+    public void guardarMonedero (Monedero monedero){
+        monederoRepository.save(monedero);
+    }
+    public void updateMonedero (Monedero monedero){
+        monederoRepository.save(monedero);
+    }
+    
+    public void eliminarMonedero (Integer id){
+        monederoRepository.deleteById(id);
+    }
+
+    public List<Monedero> allMonedero(){
+        return monederoRepository.findAll();
+    }
+    
+}
