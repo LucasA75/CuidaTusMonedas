505ac361fb5405fd3f3e045c1a70e889ab3ce842
LucasA75 <lucasacevedocastro@gmail.com>
2022-12-13 13:58:53 -0300

    react

---
+++ React CTM/src/components/index.js
@@ -0,0 +1,12 @@
+//Este archivo tiene todas las importaciones de components para tenerlos mas ordenados
+import Headermain from "./Headermain/Headermain";
+import Main from "./Main/Main";
+import MenuModulos from "./MenuModulos/MenuModulos";
+import FooterMain from "./FooterMain/FooterMain";
+
+export{
+    Headermain,
+    Main,
+    MenuModulos,
+    FooterMain
+};
