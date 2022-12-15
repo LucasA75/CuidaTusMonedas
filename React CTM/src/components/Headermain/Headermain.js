c5ec6848f1a56e3b65b16c8bad6e83900fd7708f
LucasA75 <lucasacevedocastro@gmail.com>
2022-12-15 15:54:46 -0300

    cambios en el header

--- React CTM/src/components/Headermain/Headermain.js
+++ React CTM/src/components/Headermain/Headermain.js
@@ -3,13 +3,7 @@ import { useNavigate } from "react-router-dom";
 import logo from '../../assets/logo.png';
 import "./Headermain.css"
 
-const Headermain = () => {
-            /* Palabra reservada para navegar */
-            const navigate = useNavigate();
-
-            /* Aqui hago una arrow funcion y le digo primero : donde quiero ir */
-        const handleOnClick = useCallback(()=> navigate("/IniciarSesion",{},[navigate]));
-    
+const Headermain = () => {    
     return (
         <div className="header">
             <nav class="navbar navbar-expand-lg bg-light" >
@@ -20,13 +14,12 @@ const Headermain = () => {
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
                         <div class="navbar-nav">
-                            <a class="nav-link active" aria-current="page" href="/#">Home</a>
-                            <a class="nav-link" href="/#">Conocenos</a>
+                            <a class="nav-link" href="/Conocenos">Conocenos</a>
                             <a class="nav-link" href="/#">Ayuda</a>
                             <div className="iniciarsesion">
-                            <a class="nav-link" href="/IniciarSesion" handleOnClick={handleOnClick}>Iniciar sesion</a>   
+                            <a class="nav-link" href="/IniciarSesion">Iniciar sesion</a>   
                             </div>
                         </div>
                     </div>
