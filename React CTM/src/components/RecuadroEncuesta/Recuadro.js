0498bfd3cb55b212c4b40f0ab89cd1e162dadc47
AlinBurgos <alin.burgosolivero@gmail.com>
2022-12-15 10:14:31 -0300

    encuestas

---
+++ React CTM/src/components/RecuadroEncuesta/Recuadro.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Recuadro.css";
+import "./BotonSiguiente";
+
+
+const Recuadro = () => {     
+
+    return (
+
+
+    <div class="container recuadro">
+      
+      {/* <BotonSiguiente infoBoton={"siguiente"} handleOnClick={handleOnClick} /> */}
+
+    </div>
+
+     
+  )
+}
+
+
+export default Recuadro;
+
---
+++ React CTM/src/pages/Encuesta1.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Headermain, FooterMain } from "../components";
+import Recuadro from "../components/RecuadroEncuesta/Recuadro";
+
+const Encuesta1 =() =>{
+    return(
+        <>
+        <Headermain />
+        <center>
+        <h1 class = "texto">Comencemos tu registro</h1>
+        </center>
+
+        <Recuadro />
+         </>
+    )
+}
+export default Encuesta1;
