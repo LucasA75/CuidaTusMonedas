ac9a2d1b082f7f1e2a4068712035ce5de2b8bd47
LucasA75 <lucasacevedocastro@gmail.com>
2022-12-15 09:30:07 -0300

    cambios

--- React CTM/src/App.js
+++ React CTM/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./pages/Home"
+import { Home,LoginPage } from "./pages";
 
 const App =() => {
     return(
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Home/>}></Route>
+                <Route path="/Login" element={<LoginPage/>}></Route>
             </Routes>
         </BrowserRouter>
     )
