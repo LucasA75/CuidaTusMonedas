fb06e21a40ebfd104da33799e33192714810e16a
LucasA75 <lucasacevedocastro@gmail.com>
2022-12-19 08:22:14 -0300

    Añadi un Fondo con video en Login

--- React CTM/src/pages/IniciarSesion.js
+++ React CTM/src/pages/IniciarSesion.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { FondoLogin, Login } from "../components";
+import { FondoLogin, Login,FondoVideo } from "../components";
 import "../components/Login/Login.css"
 
 const IniciarSesion = () =>{
     return(
         <main>
-        <FondoLogin/>
+        <FondoVideo/>
         <Login/>
         </main>
 
