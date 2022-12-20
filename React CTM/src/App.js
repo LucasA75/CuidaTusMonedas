2f50fb269896c36e204bb1447c16753ced974768
LucasA75 <lucasacevedocastro@gmail.com>
2022-12-20 20:03:45 -0300

    Pagina Perfil- Quedo Monona :)

--- React CTM/src/App.js
+++ React CTM/src/App.js
@@ -1,6 +1,17 @@
 import React from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import { AdminPage, IniciarSesion, Registrarse,Encuesta1,Encuesta2,Conocenos, Encuesta3, Encuesta4, ModuloP, IndexFraudesPiramidales} from "./pages";
+import { AdminPage, 
+    IniciarSesion,
+     Registrarse,
+     Encuesta1,
+     Encuesta2,
+     Conocenos, 
+     Encuesta3, 
+     Encuesta4, 
+     ModuloP, 
+     IndexFraudesPiramidales,
+    PerfilPage
+    } from "./pages";
 import Home from "./pages/Home"
 import OrigenEstafasPage from "./pages/OrigenEstafasPage";
 import PagePerso from "./pages/PagePerso";
@@ -22,6 +33,7 @@ const App =() => {
                 <Route path="/ModuloP" element={<ModuloP/>}></Route>
                 <Route path="/InPira" element={<IndexFraudesPiramidales/>}></Route>
                 <Route path="/OrigenEstafas" element={<OrigenEstafasPage/>}></Route>
+                <Route path="/Perfil" element={<PerfilPage/>}></Route>
             </Routes>
         </BrowserRouter>
     )
