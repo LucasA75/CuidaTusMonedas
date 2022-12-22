12ffbc330ea81db2fe2286175ed7ed2f42012b90
LucasA75 <lucasacevedocastro@gmail.com>
2022-12-21 23:51:39 -0300

    Pequeños cambios en el Footer

--- React CTM/src/components/FooterMain/FooterMain.js
+++ React CTM/src/components/FooterMain/FooterMain.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./FooterMain.css"
 /* import Footer from '../../assets/Footer.png'; */
 import logo from "../../assets/logo.png"
+import { Facebook, Instagram } from "@mui/icons-material";
 
 
 const FooterMain = () => {
@@ -29,8 +30,15 @@ const FooterMain = () => {
         </div>
         <div class="col columna2f">
         <h2>Preguntas Frecuentes</h2>
-          <h2>Quienes Somos</h2>
-          <h2>Nuestras Redes Sociales</h2>
+          <h2>Terminos y condiciones</h2>
+          <div>
+          <a href="https://www.facebook.com/" style={{ textDecoration: "none", color: "inherit"}}>
+            <Facebook sx={{ fontSize: 80 }}/>
+            </a>
+            <a href="https://www.instagram.com/" style={{ textDecoration: "none", color: "inherit"}}>
+            <Instagram sx={{ fontSize: 80 , marginRight:5 , marginLeft:5}}/>
+            </a>
+          </div>
         </div>
       </div>
       <div class="row row2f">
