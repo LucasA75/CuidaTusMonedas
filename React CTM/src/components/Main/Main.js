e7c3e87b38de38ca2d0797a54af92f7552fccfef
Diego Gatica <diegogaticaretamales@gmail.com>
2022-12-27 12:15:05 -0300

    popup

--- React CTM/src/components/Main/Main.js
+++ React CTM/src/components/Main/Main.js
@@ -1,13 +1,10 @@
-import React, { useCallback } from "react";
-import { useNavigate } from "react-router-dom";
-import fotomain from "../../assets/imagenMain.png"
-import "./Main.css"
+import React from "react";
+import fotomain from "../../assets/imagenMain.png";
+import "./Main.css";
+import "./Popup.js";
 
 const Main = () => {
-    const navigate = useNavigate();
-    const handleOnClick = useCallback(
-        () => navigate("/Encuesta1", {}, [navigate]))
-        
+   
     return (
         <div class="container text-center principal" >
             <div class="row align-items-start row1">
@@ -19,23 +16,28 @@ const Main = () => {
                 </div>
             </div>
             <div>
-                <button type="btn" class="btn-abrir-popup"
-                    onClick={() => {
-                        handleOnClick();
-                    }}> Comenzar a aprender</button>
+                <button type="btn" class="btn-abrir-popup" id="btn-abrir-popup"
+                    > Comenzar a aprender</button>
             </div>
 
-                    {/* implementacion de popup */}
-            
-            <div class="overlay">
-                <div class="popup">
+            {/* implementacion de popup */}
+
+            <div class="overlay" id="overlay">
+                <div class="popup" id="popup">
                     <a href="#" id="btn-cerrar-popup" class="btn-cerrar-popup"><i class="fas fa-times"></i></a>
-                    <h2 class="tituloregistrate">REGISTRATE</h2>
-                    <h4>y empieza a aprender</h4>
+                    <h2 class="tituloregistrate">ENCUESTA</h2>
+                    <h4 class="empiezaaaprender">Que te interesa aprender?</h4>
                     <form action="">
                         <div class="contenedor-inputs">
-                            <input type="text" placeholder="Nombre"/>
-						    <input type="email" placeholder="Correo"/>                           
+                        <input type="checkbox" name="box1" id="box1"/>
+                        <label for="info">Proteger tus monedas</label>
+                        <br/>
+                        <input type="checkbox" name="box2" id="box2"/>
+                        <label for="info">Administrar tus monedas</label>
+                        <br/>
+                        <input type="checkbox" name="box3" id="box3"/>
+                        <label for="info">Invertir tus monedas</label>
+                        <br/>
                         </div>
                         <input type="submit" class="btn-submit" value="Registrarse"></input>
                     </form>
@@ -43,12 +45,13 @@ const Main = () => {
 
             </div>
 
-
         </div>
 
 
     )
 }
+
+
 
 export default Main;
 
